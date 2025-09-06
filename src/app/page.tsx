d186378716d0c9fcc5c114ebfb0b03617def613e
Tyler Hartsock <thartsock02@gmail.com>
2025-09-05 19:17:36 -0500

    use revalidate path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import TestPage from "components/test-page";
 import { getRandomPokemonId, getSixRandomPokemon } from "@/lib/utils";
+import { revalidatePath } from "next/cache";
 
+revalidatePath("/");
 export default async function Home() {
   for (let i = 0; i < 6; i++) {
     const randomPokemanId = await getRandomPokemonId();
